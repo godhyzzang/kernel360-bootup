35f4d3308940019cdd71ded4b0bbd1e715fed82d
hana <yhy010116@gmail.com>
2024-07-17 14:15:13 +0900

    chore : webpack dev server 설치

--- 3-react-app/webpack.config.js
+++ 3-react-app/webpack.config.js
@@ -14,7 +14,14 @@ module.exports = {
       template: './public/index.html'
 }
   )],
-
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+    
+  },
 
   module: {
     rules: [
