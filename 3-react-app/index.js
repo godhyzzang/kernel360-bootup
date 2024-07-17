9130ffc4fe797fa50555bb0ce0942cfaef7f1f8b
hana <yhy010116@gmail.com>
2024-07-17 14:36:21 +0900

    feat :  - `index.js` 파일 작성 : `ReactDOM.createRoot()` 함수를 사용하여 Root DOM node에 `App` 컴포넌트 렌더링

--- 3-react-app/index.js
+++ 3-react-app/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.createRoot()를 사용하여 Root DOM node에 App 컴포넌트를 렌더링합니다.
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
